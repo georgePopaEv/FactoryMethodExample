72f2a2841f83daf22b39ae606fbfff724aed7dc9
GeorgePopaA <poplike14@yahoo.com>
2023-01-06 13:49:06 +0200

    Add my example

---
+++ src/AbstractShapeFactory.java
@@ -0,0 +1,28 @@
+public abstract class AbstractShapeFactory {
+
+    protected abstract Shape factoryMethod();
+
+    public Shape getShape() {
+        return factoryMethod();
+    }
+
+    //other helper methods
+}
+
+class RectangleFactory extends AbstractShapeFactory{
+    protected Shape factoryMethod(){
+        return new Rectangle();
+    }
+}
+
+class CircleFactory extends AbstractShapeFactory{
+    protected Shape factoryMethod(){
+        return new Circle();
+    }
+}
+
+class SquareFactory extends AbstractShapeFactory{
+    protected Shape factoryMethod(){
+        return new Square();
+    }
+}
